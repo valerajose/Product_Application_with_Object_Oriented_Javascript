3ab9e59f1afe3b0e7b18306a8690afa147666a40
valerajose <valera09j@gmail.com>
2021-02-19 11:32:44 -0400

    Final product form

--- App.js
+++ App.js
@@ -16,7 +16,8 @@ class UI {
                     <strong>Product Name</strong> ${product.name}
                     <strong>Product Price</strong> ${product.price}
                     <strong>Year Product</strong> ${product.year}
-                </div>
+                    <a href="#" class="btn btn-danger" name="delete">Delete</a>
+                </div> 
             </div>
         `;
         
@@ -28,29 +29,54 @@ class UI {
         document.getElementById('product-form').reset();
     }
 
-    deleteProduct() {
+    deleteProduct(element) {
+        if(element.name === 'delete') {
+            element.parentElement.parentElement.parentElement.remove();
+            this.showMessage('Product Deleted Successfully', 'info');
 
+        }
     }
 
-    showMessage() {
+    showMessage(message, cssClass) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-2`;
+        div.appendChild(document.createTextNode(message));
+
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#App');
+        container.insertBefore(div, app);
+        setTimeout(function (){
+            document.querySelector('.alert').remove();
+        }, 3000)
 
     }
 }
 
 // DOM Eventes
-
 document.getElementById('product-form')
         .addEventListener('submit', function(e){
           const name = document.getElementById('name').value;
           const price = document.getElementById('price').value;
           const year = document.getElementById('year').value;
 
-
           const product = new Product(name, price, year);
           
           const ui = new UI();
+
+          if(name === '' || price === '' || year === ''){
+              return ui.showMessage('Complete fields Please', 'danger');
+          }
           ui.addProduct(product);
-          
+          ui.resetForm();
+          ui.showMessage('Product added Successfully','success');
+
           e.preventDefault();
 
         });
+
+document.getElementById('product-list').addEventListener('click', function(e){
+    
+    const ui = new UI();
+    ui.deleteProduct(e.target);
+
+});
